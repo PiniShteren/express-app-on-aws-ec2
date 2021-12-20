3a2bd0d0690dfc4e383e121995e2fad15fe5662f
PiniShteren <pynyshteren@gmail.com>
2021-12-20 12:29:58 +0200

    p

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ThreeSixty from "react-360-view";
 import "./App.css";
 
 function App() {
@@ -7,6 +8,11 @@ function App() {
 			<div>
 				<h1>Hello to my React app</h1>
 				<p>runnig on s3 AWS with PipeLine</p>
+				{/* <ThreeSixty
+					// amount={10}
+					imagePath="https://scaleflex.cloudimg.io/width/600/q35/https://scaleflex.ultrafast.io/https://scaleflex.airstore.io/demo/chair-360-36"
+					fileName="chair_{index}.jpg?v1"
+				/> */}
 			</div>
 		</div>
 	);
