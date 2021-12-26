612575733b3b37c04b2fe8571ad3625e61fdaa04
PiniShteren <pynyshteren@gmail.com>
2021-12-26 14:22:55 +0200

    p

--- src/App.js
+++ src/App.js
@@ -8,11 +8,6 @@ function App() {
 			<div>
 				<h1>Hello to my React app</h1>
 				<p>runnig on s3 AWS with PipeLine</p>
-				{/* <ThreeSixty
-					// amount={10}
-					imagePath="https://scaleflex.cloudimg.io/width/600/q35/https://scaleflex.ultrafast.io/https://scaleflex.airstore.io/demo/chair-360-36"
-					fileName="chair_{index}.jpg?v1"
-				/> */}
 			</div>
 		</div>
 	);
